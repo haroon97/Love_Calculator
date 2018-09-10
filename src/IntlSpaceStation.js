ae3812e3905168f03e42b74b6ed3a88478daf2d3
Haroon Ahmed <haroonrmit@gmail.com>
2018-09-10 20:41:02 +0400

    Set up React Router

---
+++ src/IntlSpaceStation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class intlSpaceStation extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Space Station</h1>
+      </div>
+    );
+  };
+};
+
+export default intlSpaceStation;
---
+++ src/Routes.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import NasaImage from './NasaImage';
+import IntlSpaceStation from './IntlSpaceStation';
+import About from './About';
+import Home from './Home.js';
+import NotFound from './NotFound';
+import Header from './Header';
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Switch>
+          <Route path='/' component={Home} exact={true}/>
+          <Route path='/nasa' component={NasaImage} />
+          <Route path='/iss' component={IntlSpaceStation} />
+          <Route path='/about' component={About} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
+
+
+

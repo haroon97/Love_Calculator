072b118a294f8970779cb0b8d0456fa517c8933b
Haroon Ahmed <haroonrmit@gmail.com>
2018-09-10 20:50:07 +0400

    Added NavLinks from react-router

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <NavLink to='/' exact={true}>Home</NavLink>
+      <NavLink to='/about'>About</NavLink>
+      <NavLink to='/nasa'>Nasa IOD</NavLink>
+      <NavLink to='/iss'>Space Station</NavLink>
     </div>
   );
 };
